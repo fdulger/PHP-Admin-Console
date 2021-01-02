6675d56d69a8aa3cd81b30878f7c268ddeff0ad8
fdulger <firatdulger@gmail.com>
2021-01-02 01:05:39 +0100

    insertion works

--- v2/crud.php
+++ v2/crud.php
@@ -1,25 +1,29 @@
 <?php
 
-function item($name) {
-    return array("name" => $name);
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $strJsonFileContents = file_get_contents("data.json");
+    $data = json_decode($strJsonFileContents, true);
+    echo json_encode($data[$_GET["form"]]);
 }
 
-$db = array(
-    "Users" => array(
-        "items" => array(
-            item("Ahmet"),
-            item("Mehmet")
-        )
-    ),
-    "Cars" => array(
-        "items" => array(
-            item("Volvo"),
-            item("BMW"),
-            item("Toyota")
-        )
-    )
-);
+else if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $request = json_decode(file_get_contents('php://input'));
 
-echo json_encode($db[$_GET["form"]]);
+    $strJsonFileContents = file_get_contents("data.json");
+    $data = json_decode($strJsonFileContents, true);
+
+
+    $request->{"newElement"}->{"uuid"} = uniqid();
+
+    array_push($data[$request->{"form"}], $request->{"newElement"});
+
+    $res = json_encode($data);
+
+    $storage = fopen("data.json", "w") or die("Unable to open file!");
+    fwrite($storage, $res);
+    fclose($storage);
+
+    echo $res;
+}
 
 ?>
